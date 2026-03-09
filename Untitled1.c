d5f6946f659ca45b1bb4171b7bd0b668b47c8fca
anileser35 <anileser356@gmail.com>
2026-03-09 12:03:33 +0300

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+	printf("arac eklemek icin 1'e basiniz\n");
+	printf("araclari listelemek icin 2'ye basiniz\n");
+	printf("cikmak icin 3'e basiniz");
+	
+	int tus;
+	scanf("%d",&tus);
+	if(tus==1)
+	{
+		aracekleme();
+	} 
+	if(tus==2)
+	{
+		araclistele();
+	}
+	if(tus==3)
+	{
+		cikis();
+	}
+}
+	int aracekleme()
+	{
+		
+	}
+	
+	int araclisteleme()
+
+	int cikis()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
